55d17a75eb3bfc0fae4ce2ade4b4989db2784f5c
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-09 19:45:35 -0300

    build: create project

---
+++ src/main/java/code/blurone/chillxprewardsystem/ChillXP.kt
@@ -0,0 +1,14 @@
+package code.blurone.chillxprewardsystem
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class ChillXP : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
