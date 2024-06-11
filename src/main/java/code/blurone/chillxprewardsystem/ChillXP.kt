a820938a2f5f0b7ecbc3519908c0019a483e84b6
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-11 19:04:38 -0300

    feat(plugin): register trackers

--- src/main/java/code/blurone/chillxprewardsystem/ChillXP.kt
+++ src/main/java/code/blurone/chillxprewardsystem/ChillXP.kt
@@ -1,14 +1,55 @@
 package code.blurone.chillxprewardsystem
 
+import code.blurone.chillxprewardsystem.trackers.FarmingTracker
+import code.blurone.chillxprewardsystem.trackers.GenericBreakingTracker
+import code.blurone.chillxprewardsystem.trackers.TravelingTracker
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.Tag
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
 class ChillXP : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
-    }
+        saveDefaultConfig()
+        val cuttingDelta = config.getInt("cutting-delta", 0)
+        val diggingDelta = config.getInt("digging-delta", 0)
+        val farmingDelta = config.getInt("farming-delta", 0)
+        val miningDelta = config.getInt("mining-delta", 0)
+        val travelingDelta = config.getInt("traveling-delta", 0)
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+        if (cuttingDelta > 0)
+            server.pluginManager.registerEvents(GenericBreakingTracker(
+                Tag.LOGS.values + Tag.LEAVES.values + setOf(Material.BEEHIVE),
+                cuttingDelta,
+                NamespacedKey(this, "cut-tracker")
+            ), this)
+
+        if (diggingDelta > 0)
+            server.pluginManager.registerEvents(GenericBreakingTracker(
+                Tag.MINEABLE_SHOVEL.values - setOf(Material.SUSPICIOUS_SAND, Material.SUSPICIOUS_GRAVEL),
+                diggingDelta,
+                NamespacedKey(this, "dig-tracker")
+            ), this)
+
+        if (farmingDelta > 0)
+            server.pluginManager.registerEvents(
+                FarmingTracker(farmingDelta, NamespacedKey(this, "farm-tracker")),
+                this
+            )
+
+        if (miningDelta > 0)
+            server.pluginManager.registerEvents(GenericBreakingTracker(
+                Tag.BASE_STONE_OVERWORLD.values + Tag.BASE_STONE_NETHER.values,
+                miningDelta,
+                NamespacedKey(this, "mine-tracker")
+            ), this)
+
+        if (travelingDelta > 0)
+            server.pluginManager.registerEvents(
+                TravelingTracker(travelingDelta, NamespacedKey(this, "travel-tracker"))
+                , this
+            )
     }
 }
