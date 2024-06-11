02f2c2249dc504198c2cdd4134d158dd4367f35a
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-11 18:07:13 -0300

    feat: abstract Tracker class

---
+++ src/main/java/code/blurone/chillxprewardsystem/Tracker.kt
@@ -0,0 +1,24 @@
+package code.blurone.chillxprewardsystem
+
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.event.Listener
+import org.bukkit.persistence.PersistentDataType
+
+abstract class Tracker(
+    private val delta: Int,
+    private val trackingKey: NamespacedKey
+) : Listener {
+    companion object {
+        internal lateinit var xpGranter: XpGranter
+    }
+
+    protected fun accumulateForPlayer(player: Player, value: Double = 1.0) {
+        var accumulator = (player.persistentDataContainer.get(trackingKey, PersistentDataType.DOUBLE) ?: .0) + value
+        if (accumulator >= delta) {
+            xpGranter.grantToPlayer(player)
+            accumulator %= delta
+        }
+        player.persistentDataContainer.set(trackingKey, PersistentDataType.DOUBLE, accumulator)
+    }
+}
