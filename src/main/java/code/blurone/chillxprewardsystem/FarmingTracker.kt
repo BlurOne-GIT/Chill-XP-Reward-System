9e65f9026b71be2d2a66eb2450af714548828a1b
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-11 18:08:50 -0300

    feat(Trackers): farming

---
+++ src/main/java/code/blurone/chillxprewardsystem/FarmingTracker.kt
@@ -0,0 +1,15 @@
+package code.blurone.chillxprewardsystem
+
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerHarvestBlockEvent
+
+class FarmingTracker(
+    blocks: List<Material>,
+    delta: Int,
+    trackingKey: NamespacedKey
+) : GenericBreakingTracker(blocks, delta, trackingKey) {
+    @EventHandler
+    private fun onHarvest(event: PlayerHarvestBlockEvent) = accumulateForPlayer(event.player)
+}
