9f3d0b01166393d33e73f17c41ee37fbf620c053
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-11 18:08:11 -0300

    feat(Trackers): traveling

---
+++ src/main/java/code/blurone/chillxprewardsystem/TravelingTracker.kt
@@ -0,0 +1,15 @@
+package code.blurone.chillxprewardsystem
+
+import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerMoveEvent
+
+class TravelingTracker(delta: Int, trackingKey: NamespacedKey) : Tracker(delta, trackingKey) {
+    @EventHandler
+    private fun onPlayerTravel(event: PlayerMoveEvent) {
+        val toVector = event.to?.toVector()?.setY(0) ?: return
+        val fromVector = event.from.toVector().setY(0)
+
+        accumulateForPlayer(event.player, toVector.distance(fromVector))
+    }
+}
